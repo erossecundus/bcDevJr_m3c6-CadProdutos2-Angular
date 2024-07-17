627b855672ef32cef49e876226d866bbd5af010e
Eros Secundus <erossecundus@gmail.com>
2024-07-17 18:13:15 -0300

    Migração para Formunlário Reativo - Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +36,8 @@ registerLocaleData(ptBr);
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,30 +1,51 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Category } from '../../interfaces/Category';
 import { Product } from '../../interfaces/Product';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges {
   title = 'Formulário de Cadastro:';
 
   @Input()
-  categories : Category[] = [];
+  categories: Category[] = [];
 
   @Input()
-  product ?: Product;
+  product: Product = {} as Product;
 
   @Output()
   saveEmitter = new EventEmitter();
-  
-  constructor() {}
 
-  ngOnInit() : void {}
+  formGroupProduct: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroupProduct = this.formBuilder.group({
+      id: { value: null, disabled: true },
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      category: ['', [Validators.required]],
+      newProduct: [''],
+      promotion: ['']
+    })
+  }
+  ngOnChanges(): void {
+    if (this.product.id) {
+      this.formGroupProduct.setValue(this.product);
+    }
+  }
+
+  ngOnInit(): void { }
 
   save() {
-    this.saveEmitter.emit(true);
+    if (this.formGroupProduct.valid) {
+      Object.assign(this.product, this.formGroupProduct.value);
+      this.saveEmitter.emit(true);
+    }
   }
 
   cancel() {
@@ -34,4 +55,9 @@ export class ProductComponent {
   selectedCategory(cat1: Category, cat2: Category) {
     return cat1 && cat2 ? cat1.id === cat2.id : false;
   }
+
+  get pfgName() { return this.formGroupProduct.get("name") }
+  get pfgDescription() { return this.formGroupProduct.get("description") }
+  get pfgPrice() { return this.formGroupProduct.get("price") }
+  get pfgCategory() { return this.formGroupProduct.get("category") }
 }
